9ae32f9ba47620b8995e16c7d44370e1963f1d83
kevin olson <acidjazz@gmail.com>
2024-08-03 18:54:47 -0500

    ♻️ organize our interfaces

--- server/utils/oauth.ts
+++ server/utils/oauth.ts
@@ -3,63 +3,6 @@ import type { H3Event } from 'h3'
 import type { RuntimeConfig } from 'nuxt/schema'
 import type { BaseClient, IssuerMetadata } from 'openid-client'
 
-interface OauthProvider {
-  name: string
-  id: string
-  secret: string
-  issuer: IssuerMetadata
-  scope: string
-  callback: string
-}
-
-interface GoogleUserInfo {
-  sub: string
-  name: string
-  given_name: string
-  family_name: string
-  picture: string
-  email: string
-  email_verified: boolean
-}
-
-interface GithubUserInfo {
-  login: string
-  id: number
-  node_id: string
-  avatar_url: string
-  gravatar_id: string
-  url: string
-  html_url: string
-  followers_url: string
-  following_url: string
-  gists_url: string
-  starred_url: string
-  subscriptions_url: string
-  organizations_url: string
-  repos_url: string
-  events_url: string
-  received_events_url: string
-  type: string
-  site_admin: boolean
-  name: string
-  company: string
-  blog: string
-  location: string
-  email: string
-  hireable: boolean
-  bio: string
-  twitter_username: string
-  notification_email: string
-  public_repos: number
-  public_gists: number
-  followers: number
-  following: number
-  created_at: string
-  updated_at: string
-}
-
-interface UserInfo = GoogleUserInfo | GithubUserInfo
-
 export const oauthProviders = (cfg: RuntimeConfig): OauthProvider[] => {
   return [
     {
