fd2f36b174ef2d401f17cc832d98f7cdae083ce0
kevin olson <acidjazz@gmail.com>
2024-08-21 03:49:24 -0500

    ♻️ pull thresholds for now - include new auth file

---
+++ test/auth.ts
@@ -0,0 +1,51 @@
+import { $fetch } from '@nuxt/test-utils/e2e'
+import type { User } from '~/types/models'
+import { createUser } from '~~/server/utils/user'
+
+const users = [
+  {
+    session: {} as User,
+    cookie: '',
+    email: 'test@test.com',
+    name: 'Test User',
+    avatar: 'https://avatars.githubusercontent.com/u/31337?v=4',
+    payload: {
+      roles: {
+        admin: false,
+      },
+    },
+  },
+  {
+    session: {} as User,
+    cookie: undefined,
+    email: 'admin@test.com',
+    name: 'Admin User',
+    avatar: 'https://avatars.githubusercontent.com/u/31337?v=4',
+    payload: {
+      roles: {
+        admin: true,
+      },
+    },
+  },
+]
+
+async function setupUsers() {
+  await Promise.all(users.map(async (userData) => {
+    userData.session = await createUser(userData, 'github', {})
+  }))
+}
+
+async function actingAs(email: string) {
+  const user = users.find(user => user.email === email)
+  if (!user) throw new Error('User not found')
+  const { data } = await $fetch('/api/test/session', { method: 'POST', body: { id: user?.session?.id.toString(), hash: user?.session?.hash } })
+  user.cookie = data.cookie[1].split(';')[0] as string
+  const get = (url: string) => $fetch(url, { headers: { cookie: user.cookie as string } })
+  return { get }
+}
+
+export {
+  users,
+  setupUsers,
+  actingAs,
+}
