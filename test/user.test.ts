548c2905b1f17495ea75c5849154c68da7b8ee9c
kevin olson <acidjazz@gmail.com>
2024-08-21 18:11:41 -0500

    💚 cleaning up and passing types with helpers

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { $fetch, setup } from '@nuxt/test-utils/e2e'
-import { actingAs, users } from './auth'
+import { actingAs } from './auth'
 import { setupConfig } from './config'
 import type { MetapiResponse } from '~/types/metapi'
 import type { User } from '~/types/models'
@@ -9,33 +9,28 @@ describe('/api/me and /api/user', async () => {
   await setup(setupConfig())
   it('/api/me should 401', async () => {
     try { await $fetch('/api/me') }
-    catch (error: any) {
-      expect(error.response.status).toBe(401)
-    }
+    catch (error: any) { expect(error.response.status).toBe(401) }
   })
 
   it('get /api/me - current user session', async () => {
-    const { get } = await actingAs('test@test.com')
-    const response = await get('/api/me') as MetapiResponse<User>
-    expect(response.data.email).toEqual(users[0]?.session.email)
+    const { get, user } = await actingAs('test@test.com')
+    const response = await get<User>('/api/me')
+    expect(response.data.email).toEqual(user.session.email)
   })
 
   it ('get /api/user isAdmin: false - 404', async () => {
-    try {
-      await (await actingAs('test@test.com')).get('/api/user')
-    }
-    catch (error: any) {
-      expect(error.response.status).toBe(404)
-    }
+    try { await (await actingAs('test@test.com')).get('/api/user') }
+    catch (error: any) { expect(error.response.status).toBe(404) }
   })
 
   it ('get /api/user GET', async () => {
-    const response = await (await actingAs('admin@test.com')).get('/api/user') as MetapiResponse<User[]>
+    const response = await (await actingAs('admin@test.com')).get<User[]>('/api/user')
     expect(response.data.length).toBe(2)
   })
 
   it ('get /api/user/:id', async () => {
-    const response = await (await actingAs('admin@test.com')).get(`/api/user/${users[0]?.session.id}`) as MetapiResponse<User>
-    expect(response.data.id).toBe(users[0]?.session.id.toString())
+    const { user, get } = await actingAs('admin@test.com')
+    const response = await get<User>(`/api/user/${user.session.id}`)
+    expect(response.data.id).toBe(user.session.id.toString())
   })
 })
