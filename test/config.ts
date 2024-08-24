a960c4e420b6c862b652af92f84333fbee49fa42
kevin olson <acidjazz@gmail.com>
2024-08-23 20:58:48 -0500

    🧪 alright lets see what happens here round 3

--- test/config.ts
+++ test/config.ts
@@ -1,4 +1,5 @@
 function setupConfig() {
+  console.log('process.env.DEVRUN', process.env.DEVRUN)
   if (process.env.DEVRUN === 'true' && !process.env.CI)
     return { host: 'http://localhost:3000' }
   else
