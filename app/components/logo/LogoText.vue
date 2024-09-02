3abd9820748d9b0e1def6588cef9d5d5d7baf84e
kevin olson <acidjazz@gmail.com>
2024-09-02 18:04:40 -0500

    🚧 meh

--- app/components/logo/LogoText.vue
+++ app/components/logo/LogoText.vue
@@ -1,7 +1,12 @@
+<script lang="ts" setup>
+defineProps<{ large?: boolean }>()
+</script>
+
 <template>
-  <div class="flex items-center">
+  <div class="flex items-end">
     <div class="font-extrabold text-primary-900 dark:text-primary-100">fume</div>
-    <div class="font-extrabold text-primary-900 dark:text-primary-200 -mx-1">.</div>
+    <u-icon v-if="large" name="i-mdi-circle" class="w-12 h-12 text-primary-900 dark:text-primary-200 mb-2" />
+    <div v-else class="text-primary-900 dark:text-primary-200">.</div>
     <div class="font-extrabold text-primary-600 dark:text-primary-300">bio</div>
   </div>
 </template>
--- app/pages/label.vue
+++ app/pages/label.vue
@@ -7,12 +7,12 @@ const dims = { width: 45, height: 20 }
 
 <template>
   <div class="w-screen h-screen flex items-center justify-center">
-    <div class="flex items-center border border-white" :style="`width: ${dims.width * 4}mm; height: ${dims.height * 4}mm;` ">
-      <logo-bio class="w-52 h-52" />
+    <div class="flex items-center border border-white" :style="`width: ${dims.width * 5}mm; height: ${dims.height * 5}mm;` ">
+      <logo-bio class="w-64 h-64" />
       <div class="flex flex-col space-y-3 my-3">
-        <logo-text class="text-5xl" />
+        <logo-text class="text-9xl" large />
         <div class="bg-sky-300 text-right w-full py-2 px-6 text-3xl font-extrabold">
-          <span class="text-sky-700"> tirz </span>
+          <span class="text-sky-700"> tirzepatide </span>
           <span class="text-sky-900"> 20mg </span>
         </div>
       </div>
