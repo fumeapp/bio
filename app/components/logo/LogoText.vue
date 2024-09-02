21078112c083d43e94d9e31fe438ec441c1f4f4d
kevin olson <acidjazz@gmail.com>
2024-09-02 01:25:00 -0500

    🎨 attempt at a label

--- app/components/logo/LogoText.vue
+++ app/components/logo/LogoText.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex items-center">
     <div class="font-extrabold text-primary-900 dark:text-primary-100">fume</div>
-    <div class="font-extrabold text-primary-600 dark:text-primary-300 -ml-1">bio</div>
+    <div class="font-extrabold text-primary-900 dark:text-primary-700 -mx-1">.</div>
+    <div class="font-extrabold text-primary-600 dark:text-primary-300">bio</div>
   </div>
 </template>
---
+++ app/pages/label.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'bare',
+})
+</script>
+
+<template>
+  <div>
+    <div class="flex items-start">
+      <logo-bio class="w-52 h-52" />
+      <div class="flex flex-col space-y-3 my-3">
+        <logo-text class="text-8xl" />
+        <div class="bg-sky-300 text-right w-full py-2 px-6 text-3xl font-extrabold">
+          <span class="text-sky-700"> tirz </span>
+          <span class="text-sky-900"> 20mg </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
