e20af9e390d4fa5c5c68c84613b74d7f3cec6494
kevin olson <acidjazz@gmail.com>
2024-09-02 20:06:12 -0500

    🚧 getting there

---
+++ app/pages/privacy.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('privacy', () => queryContent('/privacy').findOne())
+if (!page.value)
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+
+useSeoMeta({
+  titleTemplate: '',
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description,
+})
+
+console.log(page)
+</script>
+
+<template>
+  <div>
+    <u-page v-if="page">
+      <UPageBody prose>
+        <ContentRenderer
+          v-if="page.body"
+          :value="page"
+        />
+      </UPageBody>
+    </u-page>
+  </div>
+</template>
+
+<style scoped></style>
