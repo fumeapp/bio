77b68bc9ede49a7296b4f49d517a55f1febf111e
kevin olson <acidjazz@gmail.com>
2024-09-08 22:36:37 -0500

    🎨 show a logo if not logged in

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -24,6 +24,15 @@ defineOgImageComponent('OgLogo')
     <div v-if="loggedIn">
       <pen-user />
     </div>
+    <div v-else class="mx-auto max-w-3xl my-12">
+      <div class="flex items-center space-x-3">
+        <logo-bio class="w-52 h-52" />
+        <div class="flex flex-col space-y-3">
+          <logo-text class="text-9xl" large />
+          <div class="text-center font-semibold uppercase tracking-[0.46em]">weight loss research resources</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
