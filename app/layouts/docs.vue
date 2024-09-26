cc0d35ca3cc9149f9cdba98cfd22e64d5bc01072
kevin olson <acidjazz@gmail.com>
2024-09-26 15:06:31 -0500

    🐛 header needs to into the docs layout

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -5,17 +5,20 @@ const navigation = inject<Ref<NavItem[]>>('navigation')
 </script>
 
 <template>
-  <u-main class="bg-gray-100 dark:bg-gray-950 overlay flex flex-col w-full">
-    <UContainer>
-      <UPage>
-        <template #left>
-          <UAside>
-            <UNavigationTree :links="mapContentNavigation(navigation[0].children)" />
-          </UAside>
-        </template>
+  <div>
+    <header-main />
+    <u-main class="flex flex-col w-full">
+      <UContainer>
+        <UPage>
+          <template #left>
+            <UAside>
+              <UNavigationTree :links="mapContentNavigation(navigation[0].children)" />
+            </UAside>
+          </template>
 
-        <slot />
-      </UPage>
-    </UContainer>
-  </u-main>
+          <slot />
+        </UPage>
+      </UContainer>
+    </u-main>
+  </div>
 </template>
